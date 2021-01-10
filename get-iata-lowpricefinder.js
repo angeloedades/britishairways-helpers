724c264db05700c347d294699fef02ef8fc95709
Angelo Edades <angeloedades@users.noreply.github.com>
2021-01-10 20:23:05 +0000

    Update get-iata-lowpricefinder.js
    Updated information, added link and added output options including price and iata route pairs

--- get-iata-lowpricefinder.js
+++ get-iata-lowpricefinder.js
@@ -1,18 +1,53 @@
 /*
-Get IATA codes in a particular tab in the Low Price Finder page
+Gets information from the British Airways low price finder page on a given tab
 
-https://aedad.es/2017/07/04/british-airways-helper-scripts-tools/
+https://www.britishairways.com/travel/low-price-finder/public/en_gb
 */
 
-$('tr.lpfDestination').each(function(){
-    console.log( $(this).attr("data-arrival-city") + ", " );
-})
-
 /*
-Example output:
+Get a list of IATA codes only present in the select tab
 
+Example output:
 WAW,
 ZAG,
 ZTH,
 ZRH,
 */
+$('tr.lpfDestination').each(function() {
+    console.log(
+        $(this).attr("data-arrival-airport") + ", "
+    );
+})
+
+/*
+Get a list of IATA codes original and destination pairs only present in the select tab
+
+Example output:
+LHR-ABZ, 
+LGW-ALC, 
+LHR-AMS, 
+LGW-AYT,
+*/
+$('tr.lpfDestination').each(function() {
+    console.log(
+        $(this).attr("data-departure-airport") + "-" +
+        $(this).attr("data-arrival-airport") + ", "
+    );
+})
+
+/*
+Get a list of IATA codes original and destination pairs only present and price in the select tab
+
+Example output:
+LHR - ABZ £31
+LGW - ALC £29
+LHR - AMS £36
+LGW - AYT £105
+*/
+$('tr.lpfDestination').each(function() {
+    console.log(
+        $(this).attr("data-departure-airport"), "-",
+        $(this).attr("data-arrival-airport"),
+        $(this).find('span.price').text()
+    );
+})
