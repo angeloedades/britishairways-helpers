f5e59e8e8fab9f836efcb7e6b992e9854d81da14
Angelo Edades <angeloedades@users.noreply.github.com>
2021-01-10 20:06:31 +0000

    Update get-cancelled-flights.js
    Updated with formatting, remove text replacement for dates and options of output

--- get-cancelled-flights.js
+++ get-cancelled-flights.js
@@ -1,11 +1,50 @@
 /*
 Get Cancelled Flights
 
-Get a list of cancelled flights from https://www.britishairways.com/rtad/travel/public/en_gb
-
-https://aedad.es/2018/02/26/british-airways-get-cancelled-flight-information/
+Get a list of cancelled flights from https:/*www.britishairways.com/rtad/travel/public/en_gb
 */
 
-$(‘tr.cancelled’).each(function() {
-    console.log($(this).children(‘td[headers=flight-header]’).text(), “-“, $(this).children(‘td[headers=from-header]’).text(), “-“, $(this).children(‘td[headers=to-header]’).text(), “- DEP:”, $(this).children(‘td[headers=dep-time-header]’).text().replace(“26 Feb”, ” 26 Feb”))
+/*
+Get a list of only BA flight numbers
+
+Example output: BA0015
+*/
+$('tr.cancelled').each(function() {
+    console.log($(this).children('td[headers=flight-header]').text())
+});
+
+/* Get a list of BA flight numbers and destinations
+
+Example output: BA0015 sSIN
+*/
+$('tr.cancelled').each(function() {
+    console.log(
+        $(this).children('td[headers=flight-header]').text(),
+        $(this).children('td[headers=to-header]').text(),
+    )
+});
+
+/* Get a list of BA flight numbers and destinations including originating airport
+
+Example output: BA0015 LHR SIN
+*/
+$('tr.cancelled').each(function() {
+    console.log(
+        $(this).children('td[headers=flight-header]').text(),
+        $(this).children('td[headers=from-header]').text(),
+        $(this).children('td[headers=to-header]').text(),
+    )
+});
+
+/* Get a list of BA flight numbers and destinations including originating airport and time with date
+
+Example output: BA0015 LHR SIN 21:50 10 JAN
+*/
+$('tr.cancelled').each(function() {
+    console.log(
+        $(this).children('td[headers=flight-header]').text(),
+        $(this).children('td[headers=from-header]').text(),
+        $(this).children('td[headers=to-header]').text(),
+        $(this).children('td[headers=dep-time-header]').text().substr(0, 5) + " " + $(this).children('td[headers=dep-time-header]').text().substr(5),
+    )
 });
